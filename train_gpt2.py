735a8b6385d81defa6234bc30ba2321b88d11110
Abbhizit GT <gabbhizit@gmail.com>
2024-12-02 00:14:03 +0530

    Added Configure_optimizer and get_lr methods

--- train_gpt2.py
+++ train_gpt2.py
@@ -177,6 +177,30 @@ class GPT(nn.Module):
 
         return model
 
+    def configure_optimizers(self, weight_decay, learning_rate, device_type):
+        # start with all of the candidate parameters (that require grad)
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        param_dict = {pn: p for pn, p in param_dict.items() if p.requires_grad}
+        # create optim groups. Any parameters that is 2D will be weight decayed, otherwise no.
+        # i.e. all weight tensors in matmuls + embeddings decay, all biases and layernorms don't.
+        decay_params = [p for n, p in param_dict.items() if p.dim() >= 2]
+        nodecay_params = [p for n, p in param_dict.items() if p.dim() < 2]
+        optim_groups = [
+            {'params': decay_params, 'weight_decay': weight_decay},
+            {'params': nodecay_params, 'weight_decay': 0.0}
+        ]
+        num_decay_params = sum(p.numel() for p in decay_params)
+        num_nodecay_params = sum(p.numel() for p in nodecay_params)
+        print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
+        print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
+        # Create AdamW optimizer and use the fused version if it is available
+        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        print(fused_available)
+        use_fused = fused_available and device_type == "mps"
+        print(f"using fused AdamW: {use_fused}")
+        
+        optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=(0.9, 0.95), eps=1e-8, fused=use_fused)
+        return optimizer
 import tiktoken
 
 class DataLoaderLite:
@@ -212,24 +236,50 @@ import time
 torch.manual_seed(1337)
 torch.mps.manual_seed(1337)
 #get logits
-model=GPT(GPTConfig)
+model=GPT(GPTConfig(vocab_size=50304))
 model.to(device)
 train_loader=DataLoaderLite(B=2,T=512)
 
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps=50
+def get_lr(it):
+    # 1) linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it+1) / warmup_steps
+    # 2) if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+    # 3) in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio)) # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+
 #optimize:
-optimizer=torch.optim.AdamW(model.parameters(),lr=0.0003)
-for i in range(50):
+#optimizer=torch.optim.AdamW(model.parameters(),lr=0.0003,betas=(0.9,0.95),eps=1e-8)
+optimizer=model.configure_optimizers(weight_decay=0.1,learning_rate=6e-4,device_type=device)
+for step in range(max_steps):
     t0=time.time()
     x,y=train_loader.next_batch()
     x,y=x.to(device),y.to(device)
     optimizer.zero_grad()
     logits,loss=model(x,y)
     loss.backward()
+    norm=torch.nn.utils.clip_grad_norm_(model.parameters(),1.0)
+    #determine and set learning rate for this iteration
+    lr= get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr']=lr
     optimizer.step()
     torch.mps.synchronize()
     t1=time.time()
     dt=(t1-t0)*1000
-    print(f"step: {i}, loss: {loss.item()}, dt: {dt:.2f}ms")
+    tokens_processed=train_loader.B * train_loader.T
+    tokens_per_sec=tokens_processed/dt
+    print(f"step: {step}, loss: {loss.item():.6f}, lr: {lr:.4e} norm: {norm:.4f}, dt: {dt:.2f}ms, tok/sec: {tokens_per_sec}")
 
 
  
